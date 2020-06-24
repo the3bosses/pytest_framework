7201c19ec63e9202df188e7d35605793c00238fd
Constant <the3bosses@gmail.com>
2020-06-24 10:00:16 +0200

    delete repo files

--- execute_tests.ps1
+++
@@ -1,3 +0,0 @@
-cd /Users/constant.vdw/PycharmProjects/pytest_tutorial
-source venv/bin/activate
-pytest
--- lib/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def create_folder(folder_name):
-    """Creates a folder if it does not exist"""
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
-    return folder_name
